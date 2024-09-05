8bff49fe1f89969f4931c80d45d228562b851332
nombre <karen.gil@tsoftglobal.com>
2024-09-05 15:37:17 -0400

    Se suben los ejercicios 2 y 3

---
+++ src/ejercicios/ParImpar.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class ParImpar {
+
+    public static void main(String[] args) {
+
+        int a = 15;
+        double residuo = a % 2;
+                if (residuo == 0) {
+        System.out.println("El numero " + a + " es par ");}
+                else
+        {System.out.println("El numero " + a + " es impar ");}
+    }
+}
---
+++ src/ejercicios/ProductoDosNumeros.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class ProductoDosNumeros {
+
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 8;
+        double producto = (a * b);
+        System.out.println("El producto de a y b da como resultado: " + producto);
+    }
+}
