ca7ca70039d2e9c85c01bdcc732539cb283c7bd0
nombre <karen.gil@tsoftglobal.com>
2024-09-05 15:23:50 -0400

    Se sube el ejercicio 1

---
+++ src/ejercicios/SumaDosNumeros.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class SumaDosNumeros {
+/*Clase que calcula la suma de dos números indicados como tipo de dato int*/
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int suma = a + b;
+        System.out.println("La suma de a y b da como resultado: " + suma);
+    }
+}
+
+
